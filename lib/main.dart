29dfa4cf1adc1466e51d464898517a9d18b9031e
Giorgio Licitra <giorgio.licitra264@edu.unito.itgit config --global user.name Giorgio>
2023-04-06 09:52:36 +0200

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-void main() => runApp(const MyApp());
+void main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
