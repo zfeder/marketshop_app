f8f76efd9af1c4770e43f9f755cd05a46a2c292e
Giorgio Licitra <giorgio.licitra264@edu.unito.itgit config --global user.name Giorgio>
2023-04-15 17:00:44 +0200

    camera added

--- lib/bottom_navigation_bar/home_bar.dart
+++ lib/bottom_navigation_bar/home_bar.dart
@@ -3,28 +3,121 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 
 class HomeBar extends StatefulWidget {
-  const HomeBar({super.key});
+  const HomeBar({Key? key}) : super(key: key);
 
   @override
   State<HomeBar> createState() => _HomeBar();
 }
 
-void signUserOut() {
-  FirebaseAuth.instance.signOut();
-}
-
 class _HomeBar extends State<HomeBar> {
+  MobileScannerController cameraController = MobileScannerController();
+  bool _screenOpened = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const IconButton(
-        onPressed: signUserOut,
-        icon: Icon(Icons.logout),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mobile Scanner"),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.yellow);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.toggleTorch(),
+          ),
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.cameraFacingState,
+              builder: (context, state, child) {
+                switch (state as CameraFacing) {
+                  case CameraFacing.front:
+                    return const Icon(Icons.camera_front);
+                  case CameraFacing.back:
+                    return const Icon(Icons.camera_rear);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.switchCamera(),
+          ),
+        ],
       ),
     );
   }
 
+  void _foundBarcode(Barcode barcode, MobileScannerArguments? args) {
+    /// open screen
+    if (!_screenOpened) {
+      final String code = barcode.rawValue ?? "---";
+      debugPrint('Barcode found! $code');
+      _screenOpened = true;
+      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+          FoundCodeScreen(screenClosed: _screenWasClosed, value: code),));
+    }
+  }
+
+  void _screenWasClosed() {
+    _screenOpened = false;
+  }
+}
+
+class FoundCodeScreen extends StatefulWidget {
+  final String value;
+  final Function() screenClosed;
+  const FoundCodeScreen({
+    Key? key,
+    required this.value,
+    required this.screenClosed,
+  }) : super(key: key);
+
+  @override
+  State<FoundCodeScreen> createState() => _FoundCodeScreenState();
+}
+
+class _FoundCodeScreenState extends State<FoundCodeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Found Code"),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            widget.screenClosed();
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_outlined,),
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Scanned Code:", style: TextStyle(fontSize: 20,),),
+              SizedBox(height: 20,),
+              Text(widget.value, style: TextStyle(fontSize: 16,),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
