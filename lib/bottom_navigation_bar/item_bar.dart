c5104109e33e2bce3bcf2ad0a7c7b0a42f8122e6
Federico Filì <32766721+zfeder@users.noreply.github.com>
2023-05-25 11:47:52 +0200

    visualizzazione dei dati in griglia

--- lib/bottom_navigation_bar/item_bar.dart
+++ lib/bottom_navigation_bar/item_bar.dart
@@ -1,4 +1,3 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -12,63 +11,69 @@ import '../firebase_options.dart';
 import 'dart:developer';
 
 class ItemBar extends StatefulWidget {
-  const ItemBar({super.key});
+  const ItemBar({Key? key}) : super(key: key);
+
   @override
-  State<ItemBar> createState() => _ItemBar();
+  _ItemBarState createState() => _ItemBarState();
 }
 
-final itemController = TextEditingController();
-
-void signUserOut() {
-  FirebaseDatabase.instance.reference().child("users").push().set({
-    "name": "John Smith",
-    "age": 30
-  });
-  log('SONO DENTROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO');
-}
-
-void cercaDb() {
-  cerca: itemController.text;
-  getDataFromDatabase();
-}
-
-List<Employee> getEmployeeData() {
-  List<Employee> employees = <Employee>[];
-  future: getDataFromDatabase();
-  builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-    if (snapshot.hasData) {
-      var showData = snapshot.data;
-      Map<dynamic, dynamic> values = showData.value;
-      List<dynamic> key = values.keys.toList();
-
-      for (int i = 0; i < key.length; i++) {
-        final data = values[key[i]];
-        employees.add(Employee(id: data['id'], name: data['name'], designation: data['designation'],
-            salary: data['salary']));
-      }
-    }
-  };
-  return employees;
-}
-
-getDataFromDatabase() async {
-  var value = FirebaseDatabase.instance.reference();
-  var getValue = await value.child('barcode/employee').once();
-  return getValue;
-}
-
-
-
-class _ItemBar extends State<ItemBar> {
-  List<Employee> employees = <Employee>[];
+class _ItemBarState extends State<ItemBar> {
+  final itemController = TextEditingController();
+  late List<Employee> employees;
   late EmployeeDataSource employeeDataSource;
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    employees = getEmployeeData();
+    employees = [];
     employeeDataSource = EmployeeDataSource(employeeData: employees);
+    getDataFromDatabase("");
   }
+
+  void signUserOut() {
+    FirebaseDatabase.instance.ref().child("users").push().set({
+      "name": "Fedeeeeee",
+      "age": 1
+    });
+    log('SONO DENTROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO');
+  }
+
+  void cercaDb() {
+    String cerca = itemController.text;
+    setState(() {
+      isLoading = true;
+      employees.clear();
+    });
+    getDataFromDatabase(cerca);
+  }
+
+  Future<void> getDataFromDatabase(String cerca) async {
+    var value = FirebaseDatabase.instance.ref();
+    var getValue = await value.child('barcode/employee').once();
+    dynamic showData = getValue.snapshot.value;
+    if (showData != null && showData is Map<dynamic, dynamic>) {
+      Map<dynamic, dynamic> dataMap = showData;
+      List<dynamic> keyList = dataMap.keys.toList();
+
+      employees = keyList.map<Employee>((key) {
+        final data = dataMap[key];
+        return Employee(
+          id: data['id'],
+          name: data['name'],
+          designation: data['designation'],
+          salary: data['salary'],
+        );
+      }).toList();
+
+      employeeDataSource = EmployeeDataSource(employeeData: employees);
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -87,58 +92,66 @@ class _ItemBar extends State<ItemBar> {
                 hintText: 'Cerca per nome',
                 obscureText: false,
               ),
-              const ElevatedButton(
-                onPressed: getEmployeeData,
-                  child: Text('Cerca')
+              ElevatedButton(
+                onPressed: cercaDb,
+                child: const Text('Cerca'),
               ),
-              SfDataGrid(
-                source: employeeDataSource,
-                columnWidthMode: ColumnWidthMode.fill,
-                columns: <GridColumn>[
-                  GridColumn(
-                      columnName: 'id',
-                      label: Container(
+              SizedBox(height: 20),
+              if (isLoading)
+                CircularProgressIndicator()
+              else
+                Expanded(
+                  child: SfDataGrid(
+                    source: employeeDataSource,
+                    columnWidthMode: ColumnWidthMode.fill,
+                    columns: <GridColumn>[
+                      GridColumn(
+                        columnName: 'id',
+                        label: Container(
                           padding: const EdgeInsets.all(16.0),
                           alignment: Alignment.center,
-                          child: const Text(
-                            'ID',
-                          ))),
-                  GridColumn(
-                      columnName: 'name',
-                      label: Container(
+                          child: const Text('ID'),
+                        ),
+                      ),
+                      GridColumn(
+                        columnName: 'name',
+                        label: Container(
                           padding: const EdgeInsets.all(8.0),
                           alignment: Alignment.center,
-                          child: const Text('Name'))),
-                  GridColumn(
-                      columnName: 'designation',
-                      label: Container(
+                          child: const Text('Name'),
+                        ),
+                      ),
+                      GridColumn(
+                        columnName: 'designation',
+                        label: Container(
                           padding: const EdgeInsets.all(8.0),
                           alignment: Alignment.center,
                           child: const Text(
                             'Designation',
                             overflow: TextOverflow.ellipsis,
-                          ))),
-                  GridColumn(
-                      columnName: 'salary',
-                      label: Container(
+                          ),
+                        ),
+                      ),
+                      GridColumn(
+                        columnName: 'salary',
+                        label: Container(
                           padding: const EdgeInsets.all(8.0),
                           alignment: Alignment.center,
-                          child: const Text('Salary'))),
-                ],
-              ),
+                          child: const Text('Salary'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
             ],
           ),
         ),
       ),
-
     );
   }
-
 }
 
-
 class EmployeeDataSource extends DataGridSource {
-  /// Creates the employee data source class with required details.
   EmployeeDataSource({required List<Employee> employeeData}) {
     _employeeData = employeeData
         .map<DataGridRow>((e) => DataGridRow(cells: [
@@ -159,16 +172,16 @@ class EmployeeDataSource extends DataGridSource {
   @override
   DataGridRowAdapter buildRow(DataGridRow row) {
     return DataGridRowAdapter(
-        cells: row.getCells().map<Widget>((e) {
-          return Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(8.0),
-            child: Text(e.value.toString()),
-          );
-        }).toList());
+      cells: row.getCells().map<Widget>((e) {
+        return Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8.0),
+          child: Text(e.value.toString()),
+        );
+      }).toList(),
+    );
   }
 }
-
 
 class Employee {
   final int id;
@@ -176,9 +189,10 @@ class Employee {
   final String designation;
   final int salary;
 
-  Employee(
-      {required this.id,
-        required this.name,
-        required this.designation,
-        required this.salary});
+  Employee({
+    required this.id,
+    required this.name,
+    required this.designation,
+    required this.salary,
+  });
 }
