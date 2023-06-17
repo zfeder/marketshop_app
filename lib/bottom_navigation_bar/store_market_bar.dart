0ad032b984ee81541e7fec04c5920a2a81d4e478
Federico Filì <32766721+zfeder@users.noreply.github.com>
2023-06-17 17:05:06 +0200

    prima implementazione google maps

--- lib/bottom_navigation_bar/store_market_bar.dart
+++ lib/bottom_navigation_bar/store_market_bar.dart
@@ -1,15 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:location/location.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 import 'api_service.dart';
-
+import 'package:geolocator/geolocator.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,12 +18,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SupermarketList(),
+      home: const SupermarketList(),
     );
   }
 }
 
 class SupermarketList extends StatefulWidget {
+  const SupermarketList({Key? key});
+
   @override
   _SupermarketListState createState() => _SupermarketListState();
 }
@@ -33,20 +36,33 @@ class _SupermarketListState extends State<SupermarketList> {
   @override
   void initState() {
     super.initState();
-    fetchSupermarkets(40.7128, -74.0060).then((data) {
+    getCurrentLocation();
+  }
+
+  void getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      double lat = position.latitude;
+      double lng = position.longitude;
+
+      List<dynamic> fetchedSupermarkets = await fetchSupermarkets(lat, lng);
+
       setState(() {
-        supermarkets = data;
+        supermarkets = fetchedSupermarkets;
       });
-    }).catchError((error) {
-      print('Errore nella richiesta API: $error');
-    });
+    } catch (e) {
+      print('Errore durante l\'ottenimento della posizione: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Supermercati Vicini'),
+        title: const Text('Supermercati Vicini'),
       ),
       body: ListView.builder(
         itemCount: supermarkets.length,
@@ -60,5 +76,26 @@ class _SupermarketListState extends State<SupermarketList> {
         },
       ),
     );
+  }
+}
+
+Future<List<dynamic>> fetchSupermarkets(double lat, double lng) async {
+  const apiKey = 'AIzaSyCKaqCw4qaOftjTRAZshAoihVZDJiDeYMI'; // Sostituisci con la tua API key di Google Places
+
+  final url =
+      'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&radius=5000&type=supermarket&key=$apiKey';
+
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+
+    if (data['status'] == 'OK') {
+      return data['results'];
+    } else {
+      throw Exception('Errore nella richiesta API: ${data['status']}');
+    }
+  } else {
+    throw Exception('Errore nella richiesta API: ${response.statusCode}');
   }
 }
